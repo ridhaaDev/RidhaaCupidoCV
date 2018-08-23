1111cf44e837ce31282d3813c1aca9035c08fd2f
ridhaaDev <ridhaadev@gmail.com>
2018-08-23 13:15:34 -0700

    Added close icon to navbar.

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,7 +2,21 @@ $(document).ready(function(){
 
 $(".visibleNav button").click(function(){
   $(".navLinks").toggle(1000);
-
+  $("i", this).toggleClass("fa-times fa-bars");
+  
 });
 
+
+$(".navLinks li a").click(function(){
+  
+  $(".navLinks").toggle(1000);
+
+
+  
 });
+});
+
+function reverticon(){
+  $("#icon").toggleClass("fa-times fa-bars");
+
+}
