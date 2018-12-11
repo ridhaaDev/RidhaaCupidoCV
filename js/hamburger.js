cab5dcfd21cac5c6a61f0934819cf7a18646f861
ridhaaDev <ridhaadev@gmail.com>
2018-12-11 15:37:43 +0200

    Fixed some alignment issues

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,22 +1,20 @@
 $(document).ready(function(){
 
-$(".visibleNav button").click(function(){
-  $(".navLinks").toggle(1000);
-  $("i", this).toggleClass("fa-times fa-bars");
-  
+	if (screen.width >= 780) {
+		console.log("Worked");
+		document.getElementById("icon").setAttribute("class", "fas fa-times");
+	} else {
+		document.getElementById("icon").setAttribute("class", "fas fa-bars");
+
+		$(".navLinks li a").click(function(){
+			$(".navLinks").toggle(1000);
+			document.getElementById("icon").setAttribute("class", "fas fa-bars");
+		});
+	}
+
+	$(".visibleNav button").click(function(){
+		$(".navLinks").slideToggle(1000);
+		$("i", this).toggleClass("fa-times fa-bars");
+	});
+	
 });
-
-
-$(".navLinks li a").click(function(){
-  
-  $(".navLinks").toggle(1000);
-
-
-  
-});
-});
-
-function reverticon(){
-  $("#icon").toggleClass("fa-times fa-bars");
-
-}
