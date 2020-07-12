37245353e6bfc20b4b362d9abd7f46c775a1a4dc
ridhaadev <ridhaadev@gmail.com>
2020-07-12 20:48:27 +0200

    updated information, fixed some bugs, changed layout of skills, xp and education, added projects

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,21 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  if (screen.width >= 780) {
+    document.getElementById("icon").setAttribute("display", "none");
+  } else {
+    document.getElementById("icon").setAttribute("class", "fas fa-bars");
+    $(".navLinks li a").click(function () {
+      $(".navLinks").toggle(1000);
+      document.getElementById("icon").setAttribute("class", "fas fa-bars");
+    });
+  }
 
-	if (screen.width >= 780) {
-		console.log("Worked");
-		document.getElementById("icon").setAttribute("display", "none");
-	} else {
-		document.getElementById("icon").setAttribute("class", "fas fa-times");
-
-		$(".navLinks li a").click(function(){
-			$(".navLinks").toggle(1000);
-			document.getElementById("icon").setAttribute("class", "fas fa-bars");
-
-		});
-	}
-
-	$(".visibleNav button").click(function(){
-		$(".navLinks").slideToggle(1000);
-		$("i", this).toggleClass("fa-times fa-bars");
-	});
-	
+  $(".visibleNav button").click(function () {
+    $(".navLinks").slideToggle(1000);
+    $("i", this).toggleClass("fa-times fa-bars");
+  });
 });
